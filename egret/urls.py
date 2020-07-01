1c044bdc159b3d7799788885548dc91a61d43ef8
scarter13 <scarter13@gmail.com>
2020-07-01 13:35:55 -0400

    adding auth tokens part two

--- egret/urls.py
+++ egret/urls.py
@@ -22,15 +22,24 @@ from api import views as api_views
 from rest_framework import routers
 from rest_framework.authtoken import views as authtoken_views
 
+
 router = routers.DefaultRouter()
 router.register('users', api_views.UserViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/auth/', include('djoser.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', authtoken_views.obtain_auth_token),
+    path('api/', include(router.urls)),
 ]
 
+
+
+urlpatterns += [
+
+]
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
