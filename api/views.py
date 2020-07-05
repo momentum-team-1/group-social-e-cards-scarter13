7f3008df0f4165afb07968aef723fef8c8ad3f60
scarter13 <scarter13@gmail.com>
2020-07-05 15:09:49 -0400

    added new path for cards/me

--- api/views.py
+++ api/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
 from rest_framework.decorators import action
+from rest_framework.response import Response
 from users.models import User
 from .models import Card
-from rest_framework import viewsets, permissions
+
 from .serializers import UserSerializer, CardSerializer
+
 
 # Create your views here.
 
@@ -20,3 +23,9 @@ class CardViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
+
+    @action(detail=False)
+    def me(self, request):
+        cards = request.user.cards.all()
+        serializer = CardSerializer(cards, many=True)
+        return Response(serializer.data)
