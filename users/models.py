16ca75b45b9c9dea14eb9ecc8676feb38a10691c
scarter13 <scarter13@gmail.com>
2020-07-06 12:50:58 -0400

    corrected conflicts in followed_user attributes on User model

--- users/models.py
+++ users/models.py
@@ -9,6 +9,6 @@ class User(AbstractUser):
     birthday = models.DateField(blank=True, null=True)
     email = models.CharField(max_length=100, null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
-    #followed_by = models.ManyToManyField(to=User, blank=True, related_name="favorite_users")
+    followed_user = models.ManyToManyField("self", blank=True, symmetrical=False, related_name="friends")
 
 
