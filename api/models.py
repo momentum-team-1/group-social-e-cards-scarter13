c1ef0aa3a3928cb228feec548e88f3762339ddd6
scarter13 <scarter13@gmail.com>
2020-07-06 22:20:01 -0400

    testing changes to font and color fields

--- api/models.py
+++ api/models.py
@@ -11,46 +11,12 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
         Token.objects.create(user=instance)
 
 class Card(models.Model):
-    COLOR_ONE = 'C_ONE'
-    COLOR_TWO = 'C_TWO'
-    #COLOR_THREE = 'C_THREE'
-    #COLOR_FOUR = 'C_FOUR'
-    #COLOR_FIVE = 'C_FIVE'
-    #COLOR_SIX = 'C_SIX'
-    #COLOR_SEVEN = 'C_SEVEN'
-    #COLOR_EIGHT = 'C_EIGHT'
-    COLOR_CHOICES =[
-        (COLOR_ONE, 'Color 1'),
-        (COLOR_TWO, 'Color 2'),
-        #(COLOR_THREE, 'Color 3'),
-        #(COLOR_FOUR, 'Color 4'),
-        #(COLOR_FIVE, 'Color 5'),
-        #(COLOR_SIX, 'Color 6'),
-        #(COLOR_SEVEN, 'Color 7'),
-        #(COLOR_EIGHT, 'Color 8'),
-    ]
-    FONT_ONE = 'F_ONE'
-    FONT_TWO = 'F_TWO'
-    #FONT_THREE = 'F_THREE'
-    #FONT_FOUR = 'F_FOUR'
-    #FONT_FIVE = 'F_FIVE'
-    #FONT_SIX = 'F_SIX'
-    #FONT_SEVEN = 'F_SEVEN'
-    #FONT_EIGHT = 'F_EIGHT'
-    FONT_CHOICES =[
-        (FONT_ONE, 'Font 1'),
-        (FONT_TWO, 'Font 2'),
-        #(FONT_THREE, 'Font 3'),
-        #(FONT_FOUR, 'Font 4'),
-        #(FONT_FIVE, 'Font 5'),
-        #(FONT_SIX, 'Font 6'),
-        #(FONT_SEVEN, 'Font 7'),
-        #(FONT_EIGHT, 'Font 8'),
-    ]
     creator = models.ForeignKey(to=User, null=True, on_delete=models.CASCADE, related_name='cards')
     title = models.CharField(max_length=100, null=True, blank=True)
-    color = models.CharField(max_length=25, choices=COLOR_CHOICES, default=COLOR_ONE)
-    font = models.CharField(max_length=25, choices=FONT_CHOICES, default=FONT_ONE)
+    #color = models.CharField(max_length=25, choices=COLOR_CHOICES, default=COLOR_ONE)
+    color = models.CharField(max_length=25, default="C_ONE")    
+    #font = models.CharField(max_length=25, choices=FONT_CHOICES, default=FONT_ONE)
+    font = models.CharField(max_length=25, default="F_ONE")    
     outer_text = models.TextField(blank=True, null=True)
     inner_text = models.TextField(blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
@@ -59,4 +25,42 @@ class Card(models.Model):
     class Meta:
         ordering = ['-date']
 
-    
+
+    """
+    #COLOR_ONE = 'C_ONE'
+    #COLOR_TWO = 'C_TWO'
+    #COLOR_THREE = 'C_THREE'
+    #COLOR_FOUR = 'C_FOUR'
+    #COLOR_FIVE = 'C_FIVE'
+    #COLOR_SIX = 'C_SIX'
+    #COLOR_SEVEN = 'C_SEVEN'
+    #COLOR_EIGHT = 'C_EIGHT'
+    #COLOR_CHOICES =[
+        #(COLOR_ONE, 'Color 1'),
+        (#COLOR_TWO, 'Color 2'),
+        #(COLOR_THREE, 'Color 3'),
+        #(COLOR_FOUR, 'Color 4'),
+        #(COLOR_FIVE, 'Color 5'),
+        #(COLOR_SIX, 'Color 6'),
+        #(COLOR_SEVEN, 'Color 7'),
+        #(COLOR_EIGHT, 'Color 8'),
+    #]
+    #FONT_ONE = 'F_ONE'
+    #FONT_TWO = 'F_TWO'
+    #FONT_THREE = 'F_THREE'
+    #FONT_FOUR = 'F_FOUR'
+    #FONT_FIVE = 'F_FIVE'
+    #FONT_SIX = 'F_SIX'
+    #FONT_SEVEN = 'F_SEVEN'
+    #FONT_EIGHT = 'F_EIGHT'
+    #FONT_CHOICES =[
+        #(FONT_ONE, 'Font 1'),
+        #(FONT_TWO, 'Font 2'),
+        #(FONT_THREE, 'Font 3'),
+        #(FONT_FOUR, 'Font 4'),
+        #(FONT_FIVE, 'Font 5'),
+        #(FONT_SIX, 'Font 6'),
+        #(FONT_SEVEN, 'Font 7'),
+        #(FONT_EIGHT, 'Font 8'),
+    #]
+    """
